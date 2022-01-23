ede3b5ac6437f8ffa98e69b6c4cb92ad803f9106
slayingMoon <g.kolev15@gmail.com>
2022-01-23 17:23:59 +0100

    added functionality to open and read files

--- src/FileFunctionality.java
+++ src/FileFunctionality.java
@@ -1,8 +1,12 @@
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class FileFunctionality {
 
     GUI gui;
+    String fileName;
+    String fileAddress;
 
     public FileFunctionality(GUI gui) {
         this.gui = gui;
@@ -18,5 +22,31 @@ public class FileFunctionality {
 
         FileDialog fd = new FileDialog(gui.window, "Open", FileDialog.LOAD);
         fd.setVisible(true);
+
+        if (fd.getFile() != null) {
+            fileName = fd.getFile();
+            fileAddress = fd.getDirectory();
+            gui.window.setTitle(fileName);
+        }
+
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileAddress + fileName)); //full file path
+
+            gui.textArea.setText(""); //clear text from previous opened document
+
+            String line = null;
+
+            while ((line = reader.readLine()) != null) {
+
+                gui.textArea.append(line + "\n");
+            }
+
+            reader.close(); //close the reader
+
+        }catch (Exception e) {
+
+            System.out.println("FILE NOT OPENED!");
+        }
     }
 }
