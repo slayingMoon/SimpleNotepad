4e93db67ed4212dce27f90ecd69decf450591bb4
slayingMoon <g.kolev15@gmail.com>
2022-01-23 10:52:17 +0100

    set close operation to exit on close

--- src/GUI.java
+++ src/GUI.java
@@ -15,5 +15,6 @@ public class GUI {
     public void createWindow() {
         window = new JFrame("Notepad");
         window.setSize(800, 600);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
