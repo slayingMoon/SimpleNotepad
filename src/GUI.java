3d88db5b0163f986e5836b3755e2cf3dd0bf554c
slayingMoon <g.kolev15@gmail.com>
2022-01-23 15:42:38 +0100

    added menu bar and menu items for File option

--- src/GUI.java
+++ src/GUI.java
@@ -5,6 +5,9 @@ public class GUI {
     JFrame window;
     JTextArea textArea;
     JScrollPane scrollPane;
+    JMenuBar menuBar;
+    JMenu menuFile, menuEdit, menuFormat, menuColor;
+    JMenuItem iNew, iOpen, iSave, iSaveAs, iExit;
 
     public static void main(String[] args) {
         new GUI();
@@ -14,6 +17,8 @@ public class GUI {
 
         createWindow();
         createTextArea();
+        createMenuBar();
+        createFileMenu();
 
         window.setVisible(true);
     }
@@ -34,5 +39,38 @@ public class GUI {
         window.add(scrollPane);
     }
 
+    public void createMenuBar() {
+        menuBar = new JMenuBar();
+        window.setJMenuBar(menuBar);
+
+        menuFile = new JMenu("File");
+        menuBar.add(menuFile);
+
+        menuEdit = new JMenu("Edit");
+        menuBar.add(menuEdit);
+
+        menuFormat = new JMenu("Format");
+        menuBar.add(menuFormat);
+
+        menuColor = new JMenu("Color");
+        menuBar.add(menuColor);
+    }
+
+    public void createFileMenu() {
+        iNew = new JMenuItem("New");
+        menuFile.add(iNew);
+
+        iOpen = new JMenuItem("Open");
+        menuFile.add(iOpen);
+
+        iSave = new JMenuItem("Save");
+        menuFile.add(iSave);
+
+        iSaveAs = new JMenuItem("Save As");
+        menuFile.add(iSaveAs);
+
+        iExit = new JMenuItem("Exit");
+        menuFile.add(iExit);
+    }
 
 }
